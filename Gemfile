33b952d057f6fd14568cf57135efde66e89acc12
vagrant <vagrant@localhost.localdomain>
2020-04-08 13:58:03 +0000

    finish layout and routes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.7.1'
 
 gem 'rails', '>= 6.0.2.2'
+gem 'bootstrap-sass'
 gem 'puma', '~> 3.11'
 gem 'sass-rails'
 gem 'uglifier', '>= 1.3.0'
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  # rootのテスト
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  # Homeページのテスト
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
+  # Helpページのテスト
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  # Aboutページのテスト
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  # Contactページのテスト
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
+
+
+
+
+
+
+
+end
