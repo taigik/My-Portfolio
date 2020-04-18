0bfffe38924697947713d4ad7feaf06d61732818
vagrant <vagrant@localhost.localdomain>
2020-04-18 09:29:05 +0000

    add user image

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,7 +14,6 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
-    assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
